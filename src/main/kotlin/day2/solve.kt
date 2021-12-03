e472d810b7a7ddcf341d66be52dbedc147943c30
Jesse Tham <github@jessetham.com>
2021-12-02 19:21:13 -0800

    Update Day 2

--- src/main/kotlin/day2/solve.kt
+++ src/main/kotlin/day2/solve.kt
@@ -2,27 +2,34 @@ package day2
 
 import java.io.File
 
-fun main() {
-    val path = File("input.txt").readLines().map { parseInput(it) }
-    // Just part 2
-    println(steer(path))
+data class Command(val direction: String, val units: Int) {
+    companion object {
+        fun of(line: String) = line.split(" ").let { Command(it[0], it[1].toInt()) }
+    }
 }
 
-fun parseInput(line: String) = line.split(" ").let { it[0] to it[1].toInt() }
+data class Submarine(val x: Int = 0, val y: Int = 0, val aim: Int = 0) {
+    fun res() = x * y
 
-fun steer(path: List<Pair<String, Int>>): Int {
-    var x = 0
-    var y = 0
-    var aim = 0
-    for ((instruction, units) in path) {
-        when (instruction) {
-            "forward" -> {
-                x += units
-                y += units * aim
-            }
-            "down" -> aim += units
-            "up" -> aim -= units
-        }
+    fun update1(command: Command) = when (command.direction) {
+        "forward" -> copy(x = x + command.units)
+        "down" -> copy(y = y + command.units)
+        "up" -> copy(y = y - command.units)
+        else -> error("unknown direction")
     }
-    return x * y
+
+    fun update2(command: Command) = when (command.direction) {
+        "forward" -> copy(x = x + command.units, y = y + command.units * aim)
+        "down" -> copy(aim = aim + command.units)
+        "up" -> copy(aim = aim - command.units)
+        else -> error("unknown direction")
+    }
+}
+
+fun main() {
+    val input = File("input.txt").readLines().map(Command::of)
+    // Part 1
+    println(input.fold(Submarine()) { sub, c -> sub.update1(c) }.res())
+    // Part 2
+    println(input.fold(Submarine()) { sub, c -> sub.update2(c) }.res())
 }
